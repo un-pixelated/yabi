6099606353836b00c104843eeb8c0c5e6b76ba71
Hardik Chona <masterhc321@gmail.com>
2026-02-23 10:43:10 +0530

    [RF] Changed #defines to ints

--- headers/constants.h
+++ headers/constants.h
@@ -7,13 +7,13 @@ in the interpreter and elsewhere so it's good to have them all in one place.
 */
 
 // I want this modifiable by the user input flags
-#define ARRAY_LENGTH 30000
+int ARRAY_LENGTH = 30000;
 
 // size in bytes, ideally 7 bits (< 1 byte) to support ASCII for now we go with 1
-#define CELL_SIZE 1
+int CELL_SIZE = 1;
 
 // size for the parentheses matching stack, also changable for your program needs
 // why 1024? God help you if you're writing a brainfuck program with more nesting depth than this.
-#define STACK_SIZE 1024
+int STACK_SIZE = 1024;
 
 #endif
