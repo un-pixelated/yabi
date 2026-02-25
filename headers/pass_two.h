3db79dcd94199a897826434727aa113be49acf46
Hardik Chona <masterhc321@gmail.com>
2026-02-25 22:57:35 +0530

    [RF] Pass two refactored into header pass_two.h

---
+++ headers/pass_two.h
@@ -0,0 +1,86 @@
+#ifndef PASS_TWO_H
+#define PASS_TWO_H
+
+#include <stdio.h>
+#include "constants.h"
+
+void interpret_brainfuck(FILE *bf_fileptr, char *cell_array, int *bracket_map) {
+        
+        int cell_ptr = 0;
+        int curr_char = 0;
+        int char_idx = 0;
+
+        while ((curr_char = fgetc(bf_fileptr)) != EOF) {
+                switch (curr_char) {
+                        case '>':
+                        // wraparound
+                        if (cell_ptr == CELL_ARRAY_LENGTH - 1) {
+                                cell_ptr = 0;
+                                char_idx++;
+                                break;
+                        }
+                        cell_ptr++;
+                        char_idx++;
+                        break;
+
+                        case '<':
+                        // wraparound
+                        if (cell_ptr == 0) {
+                                cell_ptr = CELL_ARRAY_LENGTH - 1;
+                                char_idx++;
+                                break;
+                        }
+                        cell_ptr--;
+                        char_idx++;
+                        break;
+
+                        case '+':
+                        cell_array[cell_ptr]++;
+                        // cell wraparound
+                        if (cell_array[cell_ptr] == -128) cell_array[cell_ptr] = 0;
+                        char_idx++;
+                        break;
+
+                        case '-':
+                        cell_array[cell_ptr]--;
+                        // cell wraparound
+                        if (cell_array[cell_ptr] == -1) cell_array[cell_ptr] = 127;
+                        char_idx++;
+                        break;
+
+                        case '.':
+                        printf("%c", cell_array[cell_ptr]);
+                        char_idx++;
+                        break;
+
+                        case ',':
+                        scanf("%c", &cell_array[cell_ptr]);
+                        char_idx++;
+                        break;
+                        
+                        case '[':
+                        if (cell_array[cell_ptr] == 0) {
+                                char_idx = bracket_map[char_idx] + 1;
+                                fseek(bf_fileptr, char_idx, SEEK_SET);
+                        }
+                        else char_idx++;
+                        break;
+
+                        case ']':
+                        if (cell_array[cell_ptr] != 0) {
+                                char_idx = bracket_map[char_idx] + 1;
+                                fseek(bf_fileptr, char_idx, SEEK_SET);
+                        }
+                        else char_idx++;
+                        break;
+
+                        default:
+                        // handling comments
+                        char_idx++;
+                        break;
+                }
+        }
+        printf("\n");
+}
+
+#endif
--- interpreter.c
+++ interpreter.c
@@ -4,7 +4,9 @@
 #include "./headers/constants.h"
 #include "./headers/input_validation.h"
 #include "./headers/memory_management.h"
+
 #include "./headers/pass_one.h"
+#include "./headers/pass_two.h"
 
 int main(int argc, char *argv[]) {
         if (validate_number_of_arguments(argc, argv[0]) == 1) return 1;
@@ -35,7 +37,7 @@ int main(int argc, char *argv[]) {
         // pass 1
         if (build_bracket_map(bracket_map, validation_stack, bf_fileptr) == 1) return 1;
         free_memory(validation_stack, NULL);
-        
+
         char *cell_array = calloc(CELL_ARRAY_LENGTH, CELL_SIZE);
         if (memory_allocation_check(cell_array) == 1) {
                 fclose(bf_fileptr);
@@ -45,81 +47,7 @@ int main(int argc, char *argv[]) {
         // pass 2
         rewind(bf_fileptr);
         
-        int cell_ptr = 0;
-        int curr_char = 0;
-        int char_idx = 0;
-
-        while ((curr_char = fgetc(bf_fileptr)) != EOF) {
-                switch (curr_char) {
-                        case '>':
-                        // wraparound
-                        if (cell_ptr == CELL_ARRAY_LENGTH - 1) {
-                                cell_ptr = 0;
-                                char_idx++;
-                                break;
-                        }
-                        cell_ptr++;
-                        char_idx++;
-                        break;
-
-                        case '<':
-                        // wraparound
-                        if (cell_ptr == 0) {
-                                cell_ptr = CELL_ARRAY_LENGTH - 1;
-                                char_idx++;
-                                break;
-                        }
-                        cell_ptr--;
-                        char_idx++;
-                        break;
-
-                        case '+':
-                        cell_array[cell_ptr]++;
-                        // cell wraparound
-                        if (cell_array[cell_ptr] == -128) cell_array[cell_ptr] = 0;
-                        char_idx++;
-                        break;
-
-                        case '-':
-                        cell_array[cell_ptr]--;
-                        // cell wraparound
-                        if (cell_array[cell_ptr] == -1) cell_array[cell_ptr] = 127;
-                        char_idx++;
-                        break;
-
-                        case '.':
-                        printf("%c", cell_array[cell_ptr]);
-                        char_idx++;
-                        break;
-
-                        case ',':
-                        scanf("%c", &cell_array[cell_ptr]);
-                        char_idx++;
-                        break;
-                        
-                        case '[':
-                        if (cell_array[cell_ptr] == 0) {
-                                char_idx = bracket_map[char_idx] + 1;
-                                fseek(bf_fileptr, char_idx, SEEK_SET);
-                        }
-                        else char_idx++;
-                        break;
-
-                        case ']':
-                        if (cell_array[cell_ptr] != 0) {
-                                char_idx = bracket_map[char_idx] + 1;
-                                fseek(bf_fileptr, char_idx, SEEK_SET);
-                        }
-                        else char_idx++;
-                        break;
-
-                        default:
-                        // handling comments
-                        char_idx++;
-                        break;
-                }
-        }
-        printf("\n"); 
+        interpret_brainfuck(bf_fileptr, cell_array, bracket_map);
 
         free_memory(bracket_map, cell_array, NULL);
         fclose(bf_fileptr);
